947a324dac5f076be845a4d618856ffe68359f13
ederbiason <ederbiason@hotmail.com>
2025-04-29 16:03:51 -0300

    refactor: refact backend to call api for youtube download

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
+import os
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import yt_dlp
+from dotenv import load_dotenv
+import httpx
+from typing import Optional
 
+load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
@@ -13,37 +17,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class FormatInfo(BaseModel):
-    format_id: str
-    ext: str
-    resolution: str | None
-    filesize: int | None
-    vcodec: str
-    acodec: str
+class Info(BaseModel):
+    image: str
+    title: str
 
-@app.get("/formats", response_model=list[FormatInfo])
-def get_video_formats(url: str = Query(..., description="URL do vídeo YouTube")):
-    ydl_opts = {
-        'quiet': True,
-        'skip_download': True,
-    }
+class VideoInfo(BaseModel):
+    success: bool
+    id: str
+    content: str
+    title: str
+    info: Info
+    repeat_download: Optional[bool] = None
+    message: Optional[str] = None
+    cachehash: str
+    additional_info: Optional[None] = None
+    progress_url: str
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        formats = info.get("formats", [])
+class DownloadRequest(BaseModel):
+    success: int
+    progress: int
+    download_url: str
+    text: str
+    message: str
 
-        result = []
-        for f in formats:
-            if f.get("vcodec") == "none" or f.get("acodec") == "none":  # ignora só áudio
-                continue
-            resolution = f.get("resolution") or f"{f.get('height')}p" if f.get("height") else "Unknown"
-            result.append({
-                "format_id": f["format_id"],
-                "ext": f["ext"],
-                "resolution": resolution,
-                "filesize": f.get("filesize"),
-                "vcodec": f["vcodec"],
-                "acodec": f["acodec"],
-            })
+@app.get("/api/download", response_model=VideoInfo)
+def get_video_download_info(format: str, url: str, copyright: int = 0, api: str = str(os.getenv("YOUTUBE_DOWNLOAD_KEY", ""))) -> VideoInfo:
+    api_url = os.getenv("API_DOWNLOAD_URL")
 
-        return result
+    response = httpx.get(
+        api_url,
+        params = {
+            "copyright": copyright,
+            "format": format,
+            "url": url,
+            "api": api
+        }
+    )
+
+    data = response.json()
+    print(data)
+
+    return data
