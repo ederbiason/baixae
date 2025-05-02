8678b31aac4683f37a7b55853fd3997fdf47d8bb
ederbiason <ederbiason@hotmail.com>
2025-05-02 19:07:58 -0300

    style: change header and footer styles to work properly

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export function Footer() {
     return (
-        <footer className="border-t border-textColor bg-background pb-12 md:pb-0">
-            footer
+        <footer className="w-full border-t bg-background h-14">
+            <div className="mx-5 lg:mx-10 xl:mx-20 p-5 flex items-center justify-between md:text-sm lg:text-base">
+                &copy; Eder Biason {new Date().getFullYear()}
+            </div> 
         </footer>
     )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,7 @@ import logo from "/public/assets/logo.png"
 
 export function Header() {
     return (
-        <header className="fixed bottom-0 md:top-0 md:bottom-auto left-0 w-full z-50 border-b">
+        <header className="fixed bottom-0 md:top-0 md:bottom-auto left-0 w-full z-50 border-b bg-background">
             <nav className="mx-5 lg:mx-10 xl:mx-20 px-5 py-2 flex items-center justify-between md:text-sm lg:text-base">
                 <Link href="/" className="bg-foreground rounded-full p-3">
                     <Image
